f7eaf52e9853a37ea15bda9ee970b7ce3a70f36e
Diego Amarante <diego.amarante@fielo.com>
2019-04-11 07:59:45 -0300

    New module

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,22 +1,29 @@
 ({
-	clickCreateItem : function(component, event, helper) {
-        var isFormValid = component.find("campingItemForm").reduce(function(isValid, inputCmp){
-        	inputCmp.showHelpMessageIfInvalid();    	
-            return isValid && inputCmp.get("v.validity").valid;
-        });
-        
-        if (isFormValid) {
-            var newCampingItem = JSON.parse(JSON.stringify(component.get("v.newItem")));
-            console.log(JSON.parse(JSON.stringify(newCampingItem)), JSON.stringify(newCampingItem));
-            var campingItems = JSON.parse(JSON.stringify(component.get("v.items")));
-            campingItems.push(newCampingItem);
-            component.set("v.items",campingItems);
-            component.set("v.newItem", {'Price__c': 0, 'Packed__c': false, 'Quantity__c': 0, 'Name':'', 'sobjectType': 'Camping_Item__c'});
+    clickCreateItem : function(component, event, helper) {
+        var isValidItem = component.find('campingItemForm').reduce(function(total,currentValue, index,arr)
+                                                           {
+                                                               currentValue.showHelpMessageIfInvalid();
+                                                               return total && currentValue.get('v.validity').valid;
+                                                           }, true);
+        if(isValidItem) {
+            helper.createItem(component);
         }
-        component.set("v.newItem",{'sobjectType':'Camping_Item__c',
-        'Name': '',
-        'Quantity__c': 0,
-        'Price__c': 0,
-        'Packed__c': false});
-	}
+    },
+    doInit: function(component, event, helper) {
+        var action = component.get("c.getItems");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    handleAddItem: function(component, event, helper) {
+        var item = event.getParam("item");
+        helper.updateExpense(component, item);
+    }
 })
