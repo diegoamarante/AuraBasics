5a7ef4eb313064a8f9e358495bff7ae3ace5fb5d
negro <diego.amarante@fielo.com>
2019-09-30 17:55:25 -0300

    Add Camping List refactor

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,29 +1,40 @@
 ({
-    clickCreateItem : function(component, event, helper) {
-        var isValidItem = component.find('campingItemForm').reduce(function(total,currentValue, index,arr)
-                                                           {
-                                                               currentValue.showHelpMessageIfInvalid();
-                                                               return total && currentValue.get('v.validity').valid;
-                                                           }, true);
-        if(isValidItem) {
-            helper.createItem(component);
-        }
-    },
-    doInit: function(component, event, helper) {
+    // Load items from Salesforce
+	doInit: function(component, event, helper) {
+    
+        // Create the action
         var action = component.get("c.getItems");
+    
+        // Add callback behavior for when response is received
         action.setCallback(this, function(response) {
             var state = response.getState();
-            if (state === "SUCCESS") {
+            if (component.isValid() && state == "SUCCESS") {
                 component.set("v.items", response.getReturnValue());
             }
             else {
                 console.log("Failed with state: " + state);
             }
         });
+    
+        // Send action off to be executed
         $A.enqueueAction(action);
-    },
-    handleAddItem: function(component, event, helper) {
-        var item = event.getParam("item");
-        helper.updateExpense(component, item);
-    }
+	},
+    handleAddItem : function (component,event,helper){
+	var action = component.get("c.saveItem");
+        var Item = event.getParam("item");
+        var lstItems = component.get("v.items");
+
+        lstItems.push(Item);
+        component.set("v.items",lstItems);
+        console.log("After:"+lstItems);
+        action.setParams({"CampingItem":Item});
+        action.setCallback(this,function(response){
+            var state = response.getState();
+                
+            if (component.isValid() && state === "SUCCESS") {
+                //let the magic happen
+            }
+         });
+        $A.enqueueAction(action);   
+     }
 })
